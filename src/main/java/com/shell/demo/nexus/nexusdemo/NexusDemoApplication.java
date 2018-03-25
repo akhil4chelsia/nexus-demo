683afa3bee206c776ea802010ff8a8f7760efc84
akhil.muralidharan <akhil.muralidharan@shell.com>
2018-03-25 17:31:58 +0530

    initial commit

---
+++ src/main/java/com/shell/demo/nexus/nexusdemo/NexusDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shell.demo.nexus.nexusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		SpringApplication.run(NexusDemoApplication.class, args);
+	}
+}
